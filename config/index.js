0dd6d8942457a80a4fb55df7f0bd5c070d3af965
Muhammad Habib Rohman <mhrohman@live.com>
2017-05-06 06:49:53 +0700

    fix(config): add env test config

--- config/index.js
+++ config/index.js
@@ -3,6 +3,18 @@ const config = {
   graphiqlEndpoint: '/graphiql'
 }
 
-module.exports = process.env.NODE_ENV !== 'production'
-  ? Object.assign(config, require('./dev'))
-  : Object.assign(config, require('./prod'))
+let envConfig 
+
+switch (process.env.NODE_ENV) {
+  case 'production':
+    envConfig = Object.assign(config, require('./prod'))
+    break
+  case 'test':
+    envConfig = Object.assign(config, require('./test'))
+    break
+  default:
+    envConfig = Object.assign(config, require('./dev'))
+    break;
+}
+
+module.exports = envConfig
