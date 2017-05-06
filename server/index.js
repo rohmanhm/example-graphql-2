82a6e2e50fa3bfb86b3ff2545a8d17dcc0d4b03d
Muhammad Habib Rohman <mhrohman@live.com>
2017-05-06 07:22:13 +0700

    fix(server): testable

--- server/index.js
+++ server/index.js
@@ -5,13 +5,17 @@ import express from 'express'
 import config from '@/config'
 import Middleware from './middleware'
 
-const { port } = config
+const { port, env } = config
 
 const server = express()
 
 // apply middleware to server
 Middleware(server)
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${ port }/`)
-})
+if (env !== 'test') {
+  server.listen(port, () => {
+    console.log(`Server running on http://localhost:${ port }/`)
+  })
+}
+
+export default server
